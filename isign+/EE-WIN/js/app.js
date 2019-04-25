ce1d5266315d95cf768c7638893722f81bbdd96e
tiper2066 <tiper2066@gmail.com>
2019-04-25 17:28:47 +0900

    add ee-win
    ISign+ EE-WIN 데모를 위한 웹페이지 추가

---
+++ isign+/EE-WIN/js/app.js
@@ -0,0 +1,31 @@
+$(function() {
+
+    /*--------------------------------------------------------
+        상단 메뉴 마우스 오버 시 아이콘 및 글자 색상 변경
+    ---------------------------------------------------------*/
+    var n = 0;    // 메뉴 인덱스 변수
+    $("#menu li").hover(
+        function() {
+            $(this).children("a").css({"color" : "#fff"});
+
+            n = $(this).index();
+            if( n == 0 ) {          // user 의 경우
+                $(this).children().children("img").attr({"src" : "./img/user_on.svg"})
+            } else {          // logout 의 경우
+                $(this).children().children("img").attr({"src" : "./img/logout_on.svg"})
+            }
+        },
+        function() {
+            $(this).children("a").css({"color" : "#5aaaf7"});
+
+            n = $(this).index();
+            if( n == 0 ) {        // user 의 경우
+                $(this).children().children("img").attr({"src" : "./img/user.svg"})
+            } else {             // logout 의 경우
+                $(this).children().children("img").attr({"src" : "./img/logout.svg"})
+            }
+        }
+    )
+
+
+});
